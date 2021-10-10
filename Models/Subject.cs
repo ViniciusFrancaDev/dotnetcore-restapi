04bb2c071a4e33b00003cd3ba843336000cb8508
Vinicius Franca <vfranca14@gmail.com>
2021-10-10 12:23:13 -0300

    Creating model Subject

---
+++ Models/Subject.cs
@@ -0,0 +1,10 @@
+namespace dotnetcore_restapi.Models
+{
+    public class Subject
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Summary { get; set; }
+        public string Description { get; set; }
+    }
+}
