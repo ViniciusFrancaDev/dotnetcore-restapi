886378745c10e921f825b734c03f5b2da4f4ba5b
Vinicius Franca <vfranca14@gmail.com>
2021-10-11 11:54:57 -0300

    Creating mock data from subjects

---
+++ Data/MockSubjectRepo.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using dotnetcore_restapi.Models;
+
+namespace dotnetcore_restapi.Data
+{
+    public class MockSubjectRepo : ISubjectRepo
+    {
+        public Subject GetSubjectById(int id)
+        {
+            return new Subject{Id=1, Name="Defence Against the Dark Arts",
+                Summary="Introduction that will set the foundation for your magical education",
+                Description="Class that taught students defensive techniques to defend against the Dark Arts"};
+        }
+
+        public IEnumerable<Subject> GetSubjects()
+        {
+            return new List<Subject>
+            {
+                new Subject{Id=1, Name="Defence Against the Dark Arts",
+                    Summary="Introduction that will set the foundation for your magical education",
+                    Description="Class that taught students defensive techniques to defend against the Dark Arts"},
+                new Subject{Id=2, Name="Herbology",
+                    Summary="Introduction of different elements of the field of herbology",
+                    Description="Study of magical plants and how to take care of, utilise and combat them"},
+                new Subject{Id=3, Name="Potions",
+                    Summary="Art of creating mixtures with magical effects",
+                    Description="Provides an introduction to potions and potioneering"}
+            };
+        }
+    }
+}
