12ca6c6cd7e35f2339a9de761c4cbd6e5a8afc98
Vinicius Franca <vfranca14@gmail.com>
2021-10-20 17:18:08 -0300

    Use connection string to connect sql server

--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,9 +26,12 @@ namespace dotnetcore_restapi
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<SubjectContext>(opt => opt.UseSqlServer
+                (Configuration.GetConnectionString("SubjectConnection")));
+
             services.AddControllers();
 
-            services.AddScoped<ISubjectRepo, MockSubjectRepo>();
+            services.AddScoped<ISubjectRepo, SqlSubjectRepo>();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
