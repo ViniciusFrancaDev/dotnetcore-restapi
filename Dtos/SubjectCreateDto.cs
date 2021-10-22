fe6f8bd633333c83035e9c7f49788da133c2f14a
Vinicius Franca <vfranca14@gmail.com>
2021-10-22 16:57:32 -0300

    Adding fields validation for create action

--- Dtos/SubjectCreateDto.cs
+++ Dtos/SubjectCreateDto.cs
@@ -1,11 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace dotnetcore_restapi.Dtos
 {
     public class SubjectCreateDto
     {
+        [Required]
         public string Name { get; set; }
 
+        [Required]
         public string Summary { get; set; }
 
+        [Required]
         public string Description { get; set; }
     }
 }
