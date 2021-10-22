658b480c375e9ad8541b3fadbd77c667c5911650
Vinicius Franca <vfranca14@gmail.com>
2021-10-22 16:47:22 -0300

    Creating Subject create Dto

---
+++ Dtos/SubjectCreateDto.cs
@@ -0,0 +1,11 @@
+namespace dotnetcore_restapi.Dtos
+{
+    public class SubjectCreateDto
+    {
+        public string Name { get; set; }
+
+        public string Summary { get; set; }
+
+        public string Description { get; set; }
+    }
+}
